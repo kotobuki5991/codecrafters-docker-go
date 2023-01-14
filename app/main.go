4b0d4254a54c34c2ae39200e7cf9a6e445a6bb5f
kotobuki <64915142+kotobuki5991@users.noreply.github.com>
2023-01-14 11:52:17 +0900

    1st stage

--- app/main.go
+++ app/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	// Uncomment this block to pass the first stage!
-	// "os"
-	// "os/exec"
+	"os"
+	"os/exec"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -13,16 +13,15 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage!
-	//
-	// command := os.Args[3]
-	// args := os.Args[4:len(os.Args)]
-	//
-	// cmd := exec.Command(command, args...)
-	// output, err := cmd.Output()
-	// if err != nil {
-	// 	fmt.Printf("Err: %v", err)
-	// 	os.Exit(1)
-	// }
-	//
-	// fmt.Println(string(output))
+	command := os.Args[3]
+	args := os.Args[4:len(os.Args)]
+
+	cmd := exec.Command(command, args...)
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(output))
 }
