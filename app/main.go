441d3aad803d8552e3c69e8d204df69cf66d26ac
kotobuki <64915142+kotobuki5991@users.noreply.github.com>
2023-01-14 12:04:57 +0900

    add mydocker command

--- app/main.go
+++ app/main.go
@@ -9,9 +9,6 @@ import (
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	// Uncomment this block to pass the first stage!
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
